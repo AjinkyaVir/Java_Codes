39cfebe0d69eacd782720e2c6f1722bc27ba3763
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2022-11-04 11:16:31 +0530

    sum of digits

---
+++ src/sumOfDigit/SumOfDigit.java
@@ -0,0 +1,18 @@
+package sumOfDigit;
+
+public class SumOfDigit {
+
+	public static void main(String[] args) {
+		
+		int num = 1234, sum =0;
+		
+		while(num>0) {
+			
+			sum = sum  + num % 10;
+			num = num / 10;
+			
+		}
+		System.out.println(sum);
+	}
+	
+}
