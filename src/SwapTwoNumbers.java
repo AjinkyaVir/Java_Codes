3fc30530106ad5444076b53bfb84670526897460
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2022-11-04 08:46:14 +0530

    swap two numbers

---
+++ src/SwapTwoNumbers.java
@@ -0,0 +1,19 @@
+
+public class SwapTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		//Write a Program to swap two numbers
+		
+		int a=20,b=10,temp;
+		
+		temp = a;
+		a = b;
+		b = temp;
+		
+		System.out.println("a: " + a + " " + "b: " + b);
+		
+
+	}
+
+}
