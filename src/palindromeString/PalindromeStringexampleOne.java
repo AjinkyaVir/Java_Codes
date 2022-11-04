b423cbc5cc5c1371ef76156803de061510b6826e
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2022-11-04 10:20:42 +0530

    WAP to verify given string is palindrome or not

---
+++ src/palindromeString/PalindromeStringexampleOne.java
@@ -0,0 +1,35 @@
+package palindromeString;
+
+public class PalindromeStringexampleOne {
+
+	public static void main(String[] args) {
+		//WAP to check given string is palndorme or not
+		/**
+		 * charAt() - This method used to write character at specified index value
+		 * 
+		 * equals() - This method is used to compare string content with the other string
+ 		 * 
+		 */
+		String str = "mohan";
+		String rev = "";
+		
+		for(int i=str.length()-1; i>=0; i--) {
+			
+			rev = rev + str.charAt(i);
+			
+		}
+		System.out.println(rev);
+		if(str.equals(rev)) {
+			
+			System.out.println("String is palindorme");
+			
+		}else {
+			System.out.println("String is not palindrome");
+		}
+		
+	}
+	
+	
+	
+	
+}
