855e5378669002f7413dfe8c69b24ab6e3cbab9f
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2022-11-04 09:06:09 +0530

    WAP to swap two numbers without using third variable

---
+++ src/SwapTwoNumbersExampleTwo.java
@@ -0,0 +1,18 @@
+
+public class SwapTwoNumbersExampleTwo {
+
+	public static void main(String[] args) {
+		// WAP to swap two numbers without using third variable
+		
+		int a = 100, b= 200;
+		
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+
+	}
+
+}
