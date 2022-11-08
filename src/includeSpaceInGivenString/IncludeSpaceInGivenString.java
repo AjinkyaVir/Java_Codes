c5ddaf14621667163434a5def75514b5ebc0ad94
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2022-11-08 12:50:08 +0530

    WAP to insert space in string

---
+++ src/includeSpaceInGivenString/IncludeSpaceInGivenString.java
@@ -0,0 +1,15 @@
+package includeSpaceInGivenString;
+
+public class IncludeSpaceInGivenString {
+
+	public static void main(String[] args) {
+		//WAP to insert space in string
+		
+		String str = "AjinkyaVirkud";
+		
+		System.out.println(str.substring(0, 7) + " " + str.substring(7, 13));
+		
+
+	}
+
+}
