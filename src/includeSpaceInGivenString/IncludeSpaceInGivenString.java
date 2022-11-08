3e9f6e72455c15f6101512d9638bef6125a063d6
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2022-11-08 13:08:48 +0530

    WAP to insert space in string

--- src/includeSpaceInGivenString/IncludeSpaceInGivenString.java
+++ src/includeSpaceInGivenString/IncludeSpaceInGivenString.java
@@ -7,6 +7,12 @@ public class IncludeSpaceInGivenString {
 		
 		String str = "AjinkyaVirkud";
 		
+		for(int i=0;i<=str.length()-1;i++) {
+			
+			System.out.println(i + " = " + str.charAt(i));
+			
+		}
+		System.out.println();
 		System.out.println(str.substring(0, 7) + " " + str.substring(7, 13));
 		
 
