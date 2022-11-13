3624c4d48f555a4950e43245b0ac392569fa4d44
AjinkyaVir <avirkud7@gmail.com>
2022-11-13 16:28:03 +0530

    WAP to compare two string using == operator and see the output

---
+++ src/programToCompareString/ProgramToCompareTwoString.java
@@ -0,0 +1,19 @@
+package programToCompareString;
+
+public class ProgramToCompareTwoString {
+
+	public static void main(String[] args) {
+		// WAP to compare two string using "==" operator and see the output
+		
+		String str1 = "Hello";
+		String str2 = "Madhu";
+				
+		if(str1 == str2) {
+			System.out.println("String are equal");
+		}else {
+			System.out.println("String are not equal");
+		}
+		
+	}
+
+}
