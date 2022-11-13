d6bf421edbb6afa809a2669b62112e6d8b1dc935
AjinkyaVir <avirkud7@gmail.com>
2022-11-13 09:09:11 +0530

    WAP to show use of String class method

---
+++ src/stringImportantMethdos/StringImportantMethdos.java
@@ -0,0 +1,24 @@
+package stringImportantMethdos;
+
+public class StringImportantMethdos {
+
+	public static void main(String[] args) {
+		// WAP to show use of string class important methos
+		
+		String str = "I am Ajinkya I am a QA Engineer";
+		
+		System.out.println("This length() method used find the length of the string");
+		System.out.println(str.length());
+		
+		System.out.println("charAt(index) - This method is used to print the character at given index");
+		System.out.println(str.charAt(5));
+		
+		System.out.println("toLowerCase() - This method is used to print the given string into lowe case.");
+		System.out.println(str.toLowerCase());
+		
+		System.out.println("toUpperCase() -  This method is used to print the given string into uppercase");
+		System.out.println(str.toUpperCase());
+
+	}
+
+}
