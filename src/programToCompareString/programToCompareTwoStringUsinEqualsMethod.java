0afdac22252ebeb4c97d53f5b2b14afbcf3deeaa
AjinkyaVir <avirkud7@gmail.com>
2022-11-13 16:31:09 +0530

    WAP to compare two string using equals method

---
+++ src/programToCompareString/programToCompareTwoStringUsinEqualsMethod.java
@@ -0,0 +1,20 @@
+package programToCompareString;
+
+public class programToCompareTwoStringUsinEqualsMethod {
+
+	public static void main(String[] args) {
+		// WA program To Compare Two String Using Equals Method
+		
+		String str1 = "mohan";
+		String str2 = "mohan";
+		
+		if(str1.equals(str2)) {
+			System.out.println("String are equal");
+		}else {
+			System.out.println("string are not equal");
+		}
+		
+
+	}
+
+}
