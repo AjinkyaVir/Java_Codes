bcb581c84b8f6fe9dcfea8f3edf4ace76e35a01a
AjinkyaVir <avirkud7@gmail.com>
2022-12-04 10:07:42 +0530

    WAP to find producct of two numbers

---
+++ src/javaExamplesW3Resource/ProductOfTwoNumbers.java
@@ -0,0 +1,21 @@
+package javaExamplesW3Resource;
+
+import java.util.Scanner;
+
+public class ProductOfTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter second number: ");
+		int num2 = sc.nextInt();
+		
+		System.out.println("Product of two numbers : " + (num1 * num2));
+		
+		
+
+	}
+
+}
