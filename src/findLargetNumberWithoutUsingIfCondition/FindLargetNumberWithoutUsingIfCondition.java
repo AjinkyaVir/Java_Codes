f8f62a2c906346be3e24abd9d940c115d398cc59
AjinkyaVir <avirkud7@gmail.com>
2022-12-08 18:47:50 +0530

    WAP to find Largest Number Without Using If Condition

---
+++ src/findLargetNumberWithoutUsingIfCondition/FindLargetNumberWithoutUsingIfCondition.java
@@ -0,0 +1,16 @@
+package findLargetNumberWithoutUsingIfCondition;
+
+public class FindLargetNumberWithoutUsingIfCondition {
+
+	public static void main(String[] args) {
+		// WAP to find Largest Number Without Using If Condition
+		
+		int a = 925,b = 1025;
+		
+		int largestNumber = a > b ? a : b;
+		
+		System.out.println("Largest number from give two numbers : " + largestNumber);
+
+	}
+
+}
